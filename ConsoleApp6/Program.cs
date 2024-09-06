413bb8d43d646285d06ef29d689b69326b36335d
Walfrex <michailhaziev74@gmail.com>
2024-09-06 09:40:39 +0500

    task_5

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 10;
+            double n = 1;
+            double res = 0;
+            Console.WriteLine(Task_5(x, n, res));
+
+            Console.ReadKey();
+        }
+
+        static double Task_5(double x, double n, double res)
+        {
+            for (n = 1; n < 10; n++)
+            {
+                res = Math.Pow(x, n) / (n + x);
+            }
+            return res;
+        }
+    }
+}
